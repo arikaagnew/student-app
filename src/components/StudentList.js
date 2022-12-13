66502eafa2d4f827483b1269b5490888c0edce26
arika <arikaagnew@gmail.com>
2022-12-13 11:57:44 -0500

    "started working on components"

---
+++ src/components/StudentList.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './StudentList.css';
+
+const StudentList = () => {
+    const headingClass = 'student-list__heading';
+    const listClass = 'student-list';
+
+    return (
+    <section>
+    <h2 className={headingClass}>Student List</h2>;
+      <ul className={listClass}>
+        <li>Student A</li>
+        <li>Student B</li>
+        <li>Student C</li>
+      </ul>
+    </section>
+  );
+};
+
+
+
+
+
+export default StudentList;
